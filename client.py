90efa2f36255efaaaa1f43c6b5407d894a747d97
TheNamelesswang <92366547+TheNamelesswang@users.noreply.github.com>
2022-03-13 20:55:48 +0800

    Update client.py

--- client.py
+++ client.py
@@ -10,22 +10,19 @@ url = "localhost:3457"
 channel = grpc.insecure_channel(url)
 client = FaissServiceStub(channel=channel)
 
-if __name__ == '__main__':
-    
+def add_new_items():
     # add 30000 items to Faiss
     for id in range(40000, 70000):
         emb = np.random.random([768])
         emb = json.dumps(emb.tolist())
-        result = client.add(Message_add(id = id, emb = emb))
-        if result.tag != 'add success':
-            raise RuntimeError('add fail!(may be this id has been already added)')
-        
+        client.add(Message_add(id = id, emb = emb))
 
-    # show the number of items in Faiss
+def show_faiss_size():
+     # show the number of items in Faiss
     result = client.get_size(Message_None())
     print(result)
 
-
+def recall_test():
     # recall 120 users' 200 items through their 2 latest history ids
     t1 = time.time()
     for i in range(120):
@@ -33,19 +30,17 @@ if __name__ == '__main__':
         out = client.recall_by_ids(Message_recall(his_ids = his_ids, topk = 200))
     t2 = time.time()
     print("120 users' recall time:", t2 - t1)
-    print(json.loads(out.json_str)[:3])
+    print("Format of the the first three items of the output:", json.loads(out.json_str)[:3])
 
-
+def remove_old_items():
     # remove 10000 items from Faiss
+    # tips:Due to the feature of Faiss, the id once removed and cannot be rejoined again
     for id in range(60000, 70000):
-        result = client.remove(Message_int(num = id))
-        if result.tag != 'remove success':
-            raise RuntimeError('remove fail!(may be this id has not been added)')
-    
+        client.remove(Message_int(num = id))
     result = client.get_size(Message_None())
     print("after remove's size:", result)
 
-
+def calculate_simlarity_test():
     # cal 120 user's 200 items similarity with their history from Faiss
     cal_time = 0
     for i in range(120):
@@ -56,5 +51,15 @@ if __name__ == '__main__':
         t2 = time.time()
         cal_time += t2 - t1
     print("120 users' cal time:", cal_time)
-    print(json.loads(out.json_str)[:3])
+    print("Format of the the first three items of the output:", json.loads(out.json_str)[:3])
 
+if __name__ == '__main__':
+    add_new_items()
+    show_faiss_size()
+
+    recall_test()
+    
+    remove_old_items()
+    show_faiss_size()
+
+    calculate_simlarity_test()
