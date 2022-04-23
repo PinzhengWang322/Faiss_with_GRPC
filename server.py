10d1a6e7804101feb6d1cadf64eb2dd2ae9d43d5
TheNamelesswang <92366547+TheNamelesswang@users.noreply.github.com>
2022-04-24 01:56:29 +0800

    4.24 update

--- server.py
+++ server.py
@@ -25,8 +25,10 @@ class FaissServic(FaissServiceServicer):
 
     """
     def __init__(self, Bert_dim = 768, PCA_dim = 100, train_PCA_path = './train_PCA.npy'):
+        self.day_ms = 60 * 60 * 24 * 1000
         self.Bert_dim = Bert_dim
         self.item_emb = {}
+        self.item_time = {}
         self.PCA_dim = PCA_dim
         self.PCA = faiss.PCAMatrix (Bert_dim, PCA_dim)
         X = np.load(train_PCA_path,allow_pickle=True)
@@ -36,22 +38,42 @@ class FaissServic(FaissServiceServicer):
         print(X.shape,'init Done!')
         g_log_inst._inst.info('init Done!')
 
-        file_path = os.getcwd() #获得当前工作目录
-        if os.path.exists(file_path + "/backup.npy") and os.path.exists(file_path + "/backup.index"):
+        file_path = os.getcwd()
+        
+        if os.path.exists(file_path + "/backup.txt") and os.path.exists(file_path + "/backup.index"):
             self.index = faiss.read_index("backup.index")
-            self.item_emb = np.load("backup.npy", allow_pickle=True).item()
+            f = open(file_path + "/backup.txt")
+            line = f.readline()
+
+            while line: 
+                line_dic = json.loads(line)
+                self.item_emb[line_dic['id']] = np.array(line_dic['emb'])
+                self.item_time[line_dic['id']] = line_dic['time']
+                line = f.readline() 
+            f.close() 
+
             print('load success')
+        
+        self.file = open('backup.txt', "a+")
+        
 
     def add(self, request, context):
         """ Add one item's id and this item's Bert embedding
         Args:
             request.id: The item's id
             request.Bert_embedding: The item's Bert_embedding in list form in json
+            request.time: The item's create time
 
         Returns: 
             None
         """
         id = request.id
+        if request.time == 0:
+            i_time = int(round(time.time() * 1000))
+        else:
+            i_time = request.time
+        self.item_time[id] = i_time
+
         if id in self.item_emb: 
             g_log_inst._inst.warning(str(request.id) + 'has been added')
             return Message_tag(tag = str(request.id) + 'has been added')
@@ -63,6 +85,12 @@ class FaissServic(FaissServiceServicer):
         self.index.add_with_ids(Bert_embedding, np.array((id,)).astype('int64'))
         
         g_log_inst._inst.info(str(request.id) + 'add success')
+        
+        # self.write(None, None) # If you need to add a lot of items at once, comment this line out
+
+        self.file.write(json.dumps({'id': id, 'time': i_time, 'emb': Bert_embedding.tolist()}) + '\n')
+        g_log_inst._inst.info(str(request.id) + 'backup success')
+        
         return Message_tag(tag = 'add success')
 
     def remove(self, request, context):
@@ -77,7 +105,7 @@ class FaissServic(FaissServiceServicer):
             g_log_inst._inst.warning(str(request.num) + 'has not been added, can not remove')
             return Message_tag(tag = str(request.num) + 'has not been added')
 
-        del self.item_emb[request.num]
+        # del self.item_emb[request.num]
         self.index.remove_ids(np.array([request.num], dtype=np.int64))
         g_log_inst._inst.info(str(request.num) + 'remove success')
         return Message_tag(tag = 'remove success')
@@ -99,20 +127,34 @@ class FaissServic(FaissServiceServicer):
         feat = np.zeros([1,self.PCA_dim]).astype('float32')
         for id in his_ids:
             if id not in self.item_emb:
-                g_log_inst._inst.debug('recall fail, his_ids not in faiss')
+                g_log_inst._inst.debug('recall fail, his_ids not in item_emb')
                 continue
             feat += self.item_emb[id]
         feat /= len(his_ids)
         faiss.normalize_L2(feat)
         scores, I = self.index.search(feat, request.topk)
         res = []
+        
         for dim_id, i in enumerate(I[0]):
             i = int(i)
+            
+            if request.time != 0:
+                now_ms = int(round(time.time() * 1000))
+                
+                if i not in self.item_time:
+                    g_log_inst._inst.debug('recall fail, recall_ids not in item_time')
+                    continue
+                
+                if (now_ms - self.item_time[i] > self.day_ms * request.time):
+                    g_log_inst._inst.info(str(i) + 'does not meet time standards')
+                    continue
+
             if i == -1: continue
             dic = {'id':i, 'score':scores[0][dim_id].item()} 
             # self.item_data[i]['emb'] = self.item_emb[i].tolist()
             res.append(dic)
-        g_log_inst._inst.info('recall success') 
+        
+        g_log_inst._inst.info(str(request.time) + ' recall success') 
         return Message_json(json_str = json.dumps(res))
 
     def cal_by_ids(self, request, context):
@@ -130,6 +172,7 @@ class FaissServic(FaissServiceServicer):
         for id in his_ids:
             if id not in self.item_emb:
                 g_log_inst._inst.debug('calaculate fail, his_ids not in faiss')
+                continue
             feat += self.item_emb[id]
         feat /= len(his_ids)
         faiss.normalize_L2(feat)
@@ -137,6 +180,7 @@ class FaissServic(FaissServiceServicer):
         for id in ids:
             if id not in self.item_emb:
                 g_log_inst._inst.debug('calaculate fail, ids for calculate not in faiss')
+                continue
             score = (self.item_emb[id][0] * feat).sum()
             dic = {'id':id, 'score':score.item()} 
             # self.item_data[i]['emb'] = self.item_emb[i].tolist()
@@ -146,7 +190,6 @@ class FaissServic(FaissServiceServicer):
 
     def write(self, request, context):
         faiss.write_index(self.index, 'backup.index')
-        np.save('backup.npy', self.item_emb)
         return Message_tag(tag = 'write success')
 
         
@@ -154,6 +197,7 @@ class FaissServic(FaissServiceServicer):
     
 
 def main():
+    #/home/wtc/perl5/wpz/Recall/data/new_save.npy
     grpcServer = grpc.server(futures.ThreadPoolExecutor(max_workers=30))
     add_FaissServiceServicer_to_server(FaissServic(), grpcServer)
     print(f'"msg":"grpc start @ grpc://{HOST}:{PORT}"')
