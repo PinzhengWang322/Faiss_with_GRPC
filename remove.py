f6ebdb72fe2bf1dacfcc15fea60c1cce2e82116b
TheNamelesswang <92366547+TheNamelesswang@users.noreply.github.com>
2022-04-24 17:37:11 +0800

    4.24 update+

--- remove.py
+++ remove.py
@@ -13,30 +13,36 @@ client = FaissServiceStub(channel=channel)
 
 day_ms = 60 * 60 * 24 * 1000
 
-def remove(have_removed):
+def remove():
+    file_path = os.getcwd()
+    if os.path.exists(file_path + "/have_removed.npy"):
+        index_removed, memory_removed = np.load(file_path + "/have_removed.npy", allow_pickle=True)
+    else:
+        index_removed, memory_removed = set(), set()
     now_ms = int(round(time.time() * 1000))
     file_path = os.getcwd()
     f = open(file_path + "/backup.txt")
     line = f.readline()
     while line: 
         line_dic = json.loads(line)
-        if line_dic['id'] not in have_removed and now_ms - line_dic['time'] > day_ms * 30:
-            client.remove(Message_int(num = line_dic['id']))
-            have_removed.add(line_dic['id'])
+
+        if line_dic['id'] not in index_removed and now_ms - line_dic['time'] > day_ms * 30:
+            client.remove_index(Message_int(num = line_dic['id']))
+            index_removed.add(line_dic['id'])
+
+        if line_dic['id'] not in memory_removed and now_ms - line_dic['time'] > day_ms * 180:
+            client.remove_memory(Message_int(num = line_dic['id']))
+            memory_removed.add(line_dic['id'])
+
         line = f.readline() 
+    
     f.close() 
+    np.save("have_removed.npy", [index_removed, memory_removed])
+    client.remove_memory(Message_None())
     print("remove success")
 
 def main():
-    have_removed = set()
-    remove(have_removed)
-    tomorrow_s = int(time.mktime(datetime.date.today().timetuple())) + day_ms / 1000
-    now_s = int(round(time.time()))
-    print(tomorrow_s - now_s + 3600 * 4)
-    time.sleep(tomorrow_s - now_s + 3600 * 4) #凌晨4点开始
-    while(True):
-        remove(have_removed)
-        time.sleep(day_ms / 1000)
+    remove()
     
 
 
@@ -44,3 +50,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
